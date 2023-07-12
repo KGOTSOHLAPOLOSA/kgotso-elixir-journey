d133ad8f07849fda8d8aed9976821021b634f07e
kgotsohlapolosa <kgotsohlapolosa@gmail.com>
2023-07-12 13:35:09 -0800

    Tring to figure out if i can execute two modules

--- scripts/hello.exs
+++ scripts/hello.exs
@@ -7,3 +7,17 @@ defmodule Hello do
 end
 
 Hello.world
+
+# To compile using interactive tool, just type 'iex' first. Then   'c hello.exs'
+# I can simply go into a module  and a function in it by simply  'Hello.word' on the interactive terminal
+# REMEMBER  that paranthes are optional in Elixer
+
+#--------------------------------
+#Creating a funnction that intake a name
+defmodule Hi do
+   IO.puts("Hello #{name}")
+
+end
+end
+
+#Hi.kg
